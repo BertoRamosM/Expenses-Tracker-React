d1e566976c437b2e11fe56f366bf4594e709146b
ARamos <albertramos902@gmail.com>
2024-01-31 18:09:39 +0100

    finish project

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
+
 const Transaction = ({ transactions }) => {
+
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transactions.amount < 0 ? "-" : "+"
 
   return (
     <li className={transactions.amount < 0 ? "minus" : 'plus'} key={transactions.id}>
       {transactions.text}
       <span>{sign}${Math.abs(transactions.amount)}</span>
-      <button className="delete-btn">x</button>
+      <button className="delete-btn"
+      onClick={()=>deleteTransaction(transactions.id)}>x</button>
     </li>
   );
 };
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -20,11 +20,31 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 
+  //actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    })
+  }
+
+
+   function addTransaction(transaction) {
+     dispatch({
+       type: "ADD_TRANSACTION",
+       payload: transaction,
+     });
+   }
   return (
-    <GlobalContext.Provider value={{
-      transactions: state.transactions
-    }}>
-    {children}
-  </GlobalContext.Provider>)
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
   
 }
